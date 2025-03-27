9bc120ded9a3908bc4d6128d028a52cfc4f4fc22
Mars7s <160578795+Mars7s@users.noreply.github.com>
2025-03-27 19:02:17 +0800

    Update wait.py

--- wait.py
+++ wait.py
@@ -7,7 +7,14 @@ def wait_hour():
     time.sleep(3600)
 def wait_day():
     time.sleep(86400)
-def wait(secound):
+def wait_secound_2(secound):
     time.sleep(secound)
-
-
+def wait_minute_2(minute):
+    a = minute * 60
+    time.sleep(a)
+def wait_hour_2(hour):
+    a = hour * 60 * 60
+    time.sleep(a)
+def wait_day_2(day):
+    a = minute * 60 * 60 * 60
+    time.sleep(a)
